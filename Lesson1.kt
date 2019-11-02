152940e8f391893bec97f772383045f4343a8e4d
nozawa <civaf-th123@hotmail.co.jp>
2019-11-02 11:32:23 +0900

    Lesson1

---
+++ Lesson1.kt
@@ -0,0 +1,15 @@
+/**
+ * You can edit, run, and share this code. 
+ * play.kotlinlang.org 
+ */
+
+fun main() {
+    
+    // 定数を宣言する
+    val nantoka :  String = "｡:.ﾟヽ(´∀`｡)ﾉﾟ.:｡ ゜"
+    println(nantoka)
+    
+    // 変数を宣言する
+    var varivari = 999
+    println(varivari)
+}
